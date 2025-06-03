13d307c2d00e383d8a842a9033e9245a976c1c62
JeremyJCU <jeremy.wiseman@my.jcu.edu.au>
2025-06-03 18:06:11 +1000

    Refactor greeting to use function

--- check.py
+++ check.py
@@ -1,3 +1,12 @@
 """Demo program for VCS."""
-for i in range(5):
-    print("Hello")
+
+def main():
+    """Main demo program for VCS."""
+    print_greeting()
+
+def print_greeting():
+    """Demo program for VCS."""
+    for i in range(5):
+        print("Hello")
+
+main()
